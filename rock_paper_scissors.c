f7a071e23f2ec96d8d0b30f7ad01c290649db7a5
BrianCunningham <brianc@brian-x200>
2020-01-26 11:59:08 -0500

    added seeding to rand function so that it works as intended

--- rock_paper_scissors.c
+++ rock_paper_scissors.c
@@ -1,16 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h> 
 
 int main()
 {
+    // Seeding the PRNG with the Current Time
+    srand(time(0)); 
+    
     // instructions displayed to user
     printf( "Time for a Rock-Paper-Scissors Showdown! Please enter R, P, or S.\n" );
     
     // entering showdown choices 
-    char computer_picks = rand() % 3; // this should provide a random number between 0 and 2
+    int computer_picks = rand() % 3; // this should provide a random number between 0 and 2
     char user_input = getchar();
     
-    printf("The Computer has picked %c!\n",computer_picks);
+    while (user_input != 'R' || user_input != 'r' || user_input != 'P' || user_input != 'p' || user_input != 'S' || user_input != 's'){
+        printf("Please Enter a Valid Input\n");
+        user_input = getchar();
+    }
+    
+    printf("The Computer has picked %i!\n",computer_picks);
     // game logic
 //     switch (user_input) {
 //         case 'R' || 'r':    
