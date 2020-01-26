f30149b3d494e74d825f596d74e3c87fc7fb7fc7
Brian J Cunningham <35817083+brianjcunningham@users.noreply.github.com>
2020-01-26 09:33:59 -0500

    Add files via upload
    Got the outline of the program finished. Still need to write the game logic.

---
+++ rock_paper_scissors.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    // instructions displayed to user
+    printf( "Time for a Rock-Paper-Scissors Showdown! Please enter R, P, or S.\n" );
+    
+    // entering showdown choices 
+    char computer_picks = rand() % 3;
+    char user_input = getchar();
+    
+    printf("The Computer has picked %c!\n",computer_picks);
+    // game logic
+//     switch (user_input) {
+//         case 'R' || 'r':    
+//         
+//     }
+    return 0;
+}
