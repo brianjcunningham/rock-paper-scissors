8d8d6babc19491f7055dee93bfbbd1e356c315a9
Brian Cunningham <brianc@brian-x200>
2020-01-26 09:47:25 -0500

    just commented a line to make sure I have git set up correctly

--- rock_paper_scissors.c
+++ rock_paper_scissors.c
@@ -7,7 +7,7 @@ int main()
     printf( "Time for a Rock-Paper-Scissors Showdown! Please enter R, P, or S.\n" );
     
     // entering showdown choices 
-    char computer_picks = rand() % 3;
+    char computer_picks = rand() % 3; // this should provide a random number between 0 and 2
     char user_input = getchar();
     
     printf("The Computer has picked %c!\n",computer_picks);
